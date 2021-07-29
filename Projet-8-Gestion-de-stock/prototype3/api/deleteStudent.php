486d3c2b4ab45db884fe89fff89f900ec36316e0
elyatim-omar <elyatim.omar.dev@gmail.com>
2021-07-29 10:14:15 +0100

    p8

--- Projet-8-Gestion-de-stock/prototype3/api/deleteStudent.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = $_POST["sid"];
-$dbh = new PDO("mysql:host=localhost:3306;dbname=schoolstd","root","solicode24");
-$sql = "DELETE FROM schoolstudents WHERE id = $id";
-$getStudents = $dbh->prepare($sql) ;
-$getStudents->execute();
-?>
